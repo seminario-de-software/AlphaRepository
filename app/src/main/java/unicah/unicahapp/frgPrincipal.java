edbb59470b3efa5f52d9bfcc305df2c069103fdf
JoseMunoz <jdmg93@gmail.com>
2016-04-12 21:31:22 -0600

    mejoras en el swipe refresh

--- app/src/main/java/unicah/unicahapp/frgPrincipal.java
+++ app/src/main/java/unicah/unicahapp/frgPrincipal.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +18,15 @@ import android.widget.ListView;
  */
 public class frgPrincipal extends Fragment {
 
+    // List Views
     private ListView lvRecordatorios;
     private ListView lvAnuncios;
     private ListView lvNoticias;
     // Vista principal
     private View vistaPrinciapal;
+    // swipe refresh Layout
+    private SwipeRefreshLayout refreshLayout;
+
 
     public frgPrincipal() {
         // Required empty public constructor
@@ -44,19 +49,25 @@ public class frgPrincipal extends Fragment {
         // Inflate the layout for this fragment
         vistaPrinciapal = inflater.inflate(R.layout.fragment_frg_principal, container, false);
 
+        refreshLayout =   (SwipeRefreshLayout) vistaPrinciapal.findViewById(R.id.swipeRefreshView);
         lvRecordatorios = (ListView) vistaPrinciapal.findViewById(R.id.lvRecordatorios);
         lvAnuncios =      (ListView) vistaPrinciapal.findViewById(R.id.lvAnuncios);
         lvNoticias =      (ListView) vistaPrinciapal.findViewById(R.id.lvNoticias);
 
+        getData();
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // repoblar los campos
+                getData();
+            }
+        });
+
 
         // Poblacion de las listas
-        String[] recordatorios = new String[]{"Prueba 1","Prueba 2","Prueba 3","Prueba 4","Prueba 5"};
 
-        ArrayAdapter<String>adaptadorListas =  new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,recordatorios);
 
-        lvRecordatorios.setAdapter(adaptadorListas);
-        lvAnuncios.setAdapter(adaptadorListas);
-        lvNoticias.setAdapter(adaptadorListas);
 
         return vistaPrinciapal;
 
@@ -71,5 +82,18 @@ public class frgPrincipal extends Fragment {
     public void onDestroy() {
         super.onDestroy();
     }
+
+
+    // Metodos de uso privado
+
+    private void getData(){
+        String[] recordatorios = new String[]{"Prueba 1","Prueba 2","Prueba 3","Prueba 4","Prueba 5"};
+        ArrayAdapter<String>adaptadorListas =  new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,recordatorios);
+        lvRecordatorios.setAdapter(adaptadorListas);
+        lvAnuncios.setAdapter(adaptadorListas);
+        lvNoticias.setAdapter(adaptadorListas);
+
+    }
+
 }
 
