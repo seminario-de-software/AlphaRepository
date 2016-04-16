22c639fa1a41c298f6e7365e373d9bc87169df3a
conary <conary@hotmail.es>
2016-04-15 19:08:35 -0600

    Campus individual SAN PEDRO

---
+++ app/src/main/java/unicah/unicahapp/frgCampusSanPedro.java
@@ -0,0 +1,29 @@
+package unicah.unicahapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class frgCampusSanPedro extends Fragment {
+
+
+    public frgCampusSanPedro() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frg_campus_san_pedro, container, false);
+    }
+
+}
